9a1db4d0f5b30450418e9657bc6fc3354b184456
Jesper Niekerk <jespervanniekerk1@gmail.com>
2026-03-23 11:20:53 +0100

    2w

--- project1/src/app/App.jsx
+++ project1/src/app/App.jsx
@@ -3,6 +3,7 @@ import { AboutMe } from '../aboutme/aboutme';
 import { Top10 } from '../top10/Top10';
 import { Card } from '../card/Card';
 import { Cookie } from '../cookie/cookie';
+import { Cat } from '../cat/cat';
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
       <Card name="jap2"/>
       <Card name="jap3"/>
       <Card name="jap4"/>
+      <Cat/>
     </>
   )
 }
